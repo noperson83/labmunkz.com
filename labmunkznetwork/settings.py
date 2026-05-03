444767446edb2f373cff7e34bdb10d81e9774444
noperson83 <marcus@noperson.com>
2026-05-03 08:49:08 +0000

    Collect project static assets on deploy

--- labmunkznetwork/settings.py
+++ labmunkznetwork/settings.py
@@ -144,6 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # Media settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
